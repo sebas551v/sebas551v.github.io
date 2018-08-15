2c4e6fdaa57ab7e5782cb96529063cbf4e53e394
sebas551v <36936886+sebas551v@users.noreply.github.com>
2018-08-15 15:03:41 -0500

    Biografía

---
+++ index.js
@@ -0,0 +1,11 @@
+function hello() {
+    alert("Hola")
+}
+
+function nuevo(){
+    var objectName = document.getElementById('txtName')
+    var txtName = document.getElementById('txtName').value
+    console.log(objectName)
+    console.log(txtName)
+    alert('Hola Mundo')
+}
